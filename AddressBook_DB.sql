916d0c65749e40aa14b1fc6d12f7abe88633a8f8
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-29 14:34:56 +0530

    [Kanishk] added:UC3 to add new contacts in table

--- AddressBook_DB.sql
+++ AddressBook_DB.sql
@@ -17,4 +17,8 @@ craete table addressbook
    email_id    	 varchar(100) not null,
    primary key   (email_id)
 );
-
+#ability to insert new contacts
+ insert into addressbook
+    (first_name,last_name,address,city,state,zip,phone_number,email_id)
+    values('Kanishk','Kumar','Sh-3/40','Varanasi','UP',221003,89385384,'kasn@gmail.com'),
+    ('Adarsh','Amasn','sds-34','Giridih','Bihar',4645,767564545,'asas@gmail.com');
