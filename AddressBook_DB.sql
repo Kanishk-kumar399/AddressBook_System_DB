878c51069141e4c9f698079d1fe145c85fd409a4
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-10-29 14:26:31 +0530

    [Kanishk] added:UC2 to create table in given DB

--- AddressBook_DB.sql
+++ AddressBook_DB.sql
@@ -3,3 +3,18 @@
 #Create database and use it
 create database addressbook-service;
 use addressbook_service;
+
+#create addressBook table
+craete table addressbook
+(
+   first_name    varchar(150) not null,
+   last_name     varchar(150) not null,
+   address       varchar(400), 
+   city	   	 varchar(20),
+   state	 varchar(20),
+   zip		 int unsigned,
+   phone_number	 int unsigned,
+   email_id    	 varchar(100) not null,
+   primary key   (email_id)
+);
+
